3e802ab711bcc415cbae89a3b1b3e06734834651
Adavize Hassan <adavize302@gmail.com>
2025-06-21 04:56:56 +0100

    refactor: 💡 update admin

--- apps/admin/app.vue
+++ apps/admin/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex gap-4">
-    <h1 class="text-3xl font-medium">Admin app</h1>
+    <h1 class="text-3xl font-medium">Adminapp</h1>
     <UButton>Login</UButton>
   </div>
 </template>
